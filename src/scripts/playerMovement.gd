ace336ba89bdd3ebafbcb0ff36426fda3a923e60
tobi.wurzer <tobias@q1studios.at>
2026-04-25 01:21:51 +0200

    add basic movement #rocket

--- src/scripts/playerMovement.gd
+++ src/scripts/playerMovement.gd
@@ -5,18 +5,19 @@ extends Node
 @export var model_container : Node3D 
 
 @export_group("Running")
-@export var max_speed := 10.0            
-@export var acceleration := 50.0
-@export var deceleration := 70.0
+@export var max_speed := 100.0            
+@export var acceleration := 500.0
+@export var deceleration := 700.0
+@export var autoDeceleration := 200.0
 
 @export_group("Jump")
 @export var allowDoubleJump := true
-@export var jumpForce := 12.0
-@export var airAcceleration := 20.0
-@export var airDeceleration := 10.0
+@export var jumpForce := 300.0
+@export var airAcceleration := 200.0
+@export var airDeceleration := 100.0
 
 @export_group("La Physics")
-@export var gravitationalConstant = 9.81
+@export var gravitationalConstant = 98.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,8 +25,20 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	_simulate_gravity(delta)
+	_handleInput(delta)
 	
 	player.move_and_slide()
+	
+	
+func _handleInput(delta: float) -> void:
+	var direction = Input.get_axis("moveLeft", "moveRight")
+	if direction:
+		player.velocity.x = direction * max_speed
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, autoDeceleration)
+	
+	if Input.is_action_just_pressed("jump"):
+		player.velocity.y -= jumpForce
 	
 	
 func _simulate_gravity(delta: float) -> void:
