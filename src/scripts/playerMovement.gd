cac01442345e7027e15e0c573860214ebf004f36
tobi.wurzer <tobias@q1studios.at>
2026-04-24 23:37:17 +0200

    setup sample scene

---
+++ src/scripts/playerMovement.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export_group("Character & Model")
+@export var player : CharacterBody3D 
+@export var model_container : Node3D 
+
+@export_group("Running")
+@export var max_speed := 10.0            
+@export var acceleration := 50.0
+@export var deceleration := 70.0
+
+@export_group("Sprint")
+@export var booooooooooost_acceleration := 5.0
+@export var max_speed_while_boosting := 12.5
+@export var max_stamina = 2.0
+@export var stamina_cooldown = 4.0
+
+@export_group("Jump")
+@export var allowDoubleJump := true
+@export var jumpForce := 12.0
+@export var airAcceleration := 20.0
+@export var airDeceleration := 10.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
