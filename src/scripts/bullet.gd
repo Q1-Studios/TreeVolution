e0d13a696440027b9d1c1df979d66ec9872e57d0
wolphi <phiwolfger@edu.aau.at>
2026-04-25 02:29:14 +0200

    added basic gun handling

---
+++ src/scripts/bullet.gd
@@ -0,0 +1,49 @@
+extends RigidBody2D
+
+var velocity: Vector2 = Vector2.ZERO
+var fired = false
+var bounce_amount: int = 1
+var size: float = 1.0
+
+func spawn(_position: Vector2, _direction: float, _velocity: Vector2, _size: float):
+	position = _position
+	rotation = _direction
+	velocity = _velocity
+	size = _size
+	
+func set_bounces(amount: int):
+	bounce_amount = amount
+	
+func update_size():
+	scale = Vector2(size, size)
+	
+func move(delta: float):
+	# apply impulse only once
+	if !fired:
+		apply_central_impulse(velocity * delta)
+		fired = true
+	
+func check_for_collision():
+	# check for colliding bodies
+	bounce_amount = bounce_amount - get_contact_count()
+		
+	# if bounce amount is hit, remove child from tree
+	if bounce_amount <= 0:
+		queue_free()
+		return
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_contacts_reported = 1
+	contact_monitor = true
+	update_size()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if is_queued_for_deletion():
+		return
+	
+	update_size()
+	check_for_collision()
+	move(delta)
---
+++ src/scripts/gun.gd
@@ -0,0 +1,51 @@
+extends StaticBody2D
+
+var Bullet = preload("res://src/scenes/bullet.tscn")
+
+@export_group("Weapon Stats")
+@export var damage: float = 3
+@export var attack_cooldown: float = 0.5
+
+@export_group("Bullet Attributes")
+@export var bullet_count: int = 1
+@export var bullet_bounces: int = 1
+@export var bullet_size: float = 0.4
+@export var bullet_velocity: float = 4000
+
+var can_shoot: bool = true;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+		
+func _physics_process(_delta: float) -> void:
+	rotate_weapon()
+	
+	if(Input.is_action_pressed("shoot")):
+		shoot()
+	
+func rotate_weapon():
+	var dir = get_global_mouse_position() - global_position
+	if dir.length() > 5:
+		rotation = dir.angle()
+		
+func start_shooting_cooldown():
+		await get_tree().create_timer(attack_cooldown).timeout
+		can_shoot = true
+
+func spawn_bullet():
+	var dir = get_global_mouse_position() - global_position
+	var velocity = Vector2(bullet_velocity, 0).rotated(dir.angle())
+		
+	var bullet = Bullet.instantiate()
+	bullet.spawn($Muzzle.global_position, dir.angle(), velocity, bullet_size)
+	bullet.set_bounces(bullet_bounces)
+	get_tree().root.add_child(bullet)
+
+func shoot():
+	if !can_shoot:
+		return
+		
+	can_shoot = false
+	spawn_bullet()
+	start_shooting_cooldown()
