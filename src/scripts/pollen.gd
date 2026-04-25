630bba71f7d21da197482d1778abb8f8abe518a3
XtophB <xtophb@gmail.com>
2026-04-25 19:43:35 +0200

    feat: pollen can modulate color based on how many pollen upgrades

--- src/scripts/pollen.gd
+++ src/scripts/pollen.gd
@@ -7,15 +7,19 @@ var pollen_damage_enemy_amount:int = 5
 var pollen_ability_cooldown:int = 10
 var pollen_life_time: int = 10
 
+var pollen_heal_upgrade_count: float = 1
+var pollen_damage_upgrade_count: float = 3
+var pollen_blocking_unlock:float = min(pollen_heal_upgrade_count, pollen_damage_upgrade_count)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Sprite2D.modulate = Color(pollen_damage_upgrade_count, pollen_heal_upgrade_count, pollen_blocking_unlock)
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 
 func _on_bullet_2d_body_entered(body: Node2D) -> void:
