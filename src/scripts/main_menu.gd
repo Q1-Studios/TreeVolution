f53aff52c75e949ee4ebe88f45405cb9bbf1d547
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-05-28 16:17:30 +0200

    Display restart hint after quitting on web builds

--- src/scripts/main_menu.gd
+++ src/scripts/main_menu.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+@export var web_restart_info: ColorRect
+
 func _on_play_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/map.tscn")
 
 func _on_cool_button_2_button_up() -> void:
+	if OS.get_name() == "Web":
+		web_restart_info.show()
 	get_tree().quit(0)
